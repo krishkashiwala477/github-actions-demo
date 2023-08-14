0b992282057cd1d81c7e0f689f7699eeaad29e31
krishkashiwala477 <krish@heliverse.com>
2023-08-14 23:22:30 +0530

    chages

---
+++ build.js
@@ -0,0 +1,9 @@
+const fs = require("fs-extra");
+
+// Clear the dist folder if it exists
+fs.emptyDirSync("dist");
+
+// Copy files from src to dist
+fs.copySync("src", "dist");
+
+console.log("Build completed.");
---
+++ main.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .json(
+      "successfully setup this node.js server with ec2 and github actions."
+    );
+});
+
+app.listen(9000, () => console.log("server started at 9000"));
