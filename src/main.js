80955f34ae8a253cb5de4ec435f45ac514b4a935
krishkashiwala477 <krish@heliverse.com>
2023-08-15 22:37:11 +0530

    first test ga run

--- src/main.js
+++ src/main.js
@@ -8,5 +8,11 @@ app.get("/", (req, res) => {
       "successfully setup this node.js server with ec2 and github actions."
     );
 });
-
+app.get("/test", (req, res) => {
+  return res
+    .status(200)
+    .json(
+      "Man you have nailed have confidence on yourself god will help you and you will succed in life."
+    );
+});
 app.listen(9000, () => console.log("server started at 9000"));
